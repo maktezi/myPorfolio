2745fb47b314f111efe3ed4cab2a034497e233d2
Mak Tezi <94935785+maktezi@users.noreply.github.com>
2024-02-15 13:46:03 +0800

    update download button, images links and etc

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,6 +1,23 @@
 import Button from '@mui/material/Button';
+import resumepdf from '/src/assets/resume.pdf';
+import indeed from '/src/assets/img/indeed.png';
+import github from '/src/assets/img/github.png';
+import fb from '/src/assets/img/fb.png';
 
 const Profile = () => {
+
+    const handleDownload = () => {
+
+        const resume = resumepdf;
+        
+        const link = document.createElement('a');
+            link.href = resume;
+            link.setAttribute('download', 'resume.pdf');
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+    };
+
     return (
     <>
     <div className='profile-detail-wrapper'>
@@ -9,13 +26,19 @@ const Profile = () => {
             <h1 className="profile-name">MARK</h1>
             <h3 className="profile-skill">Web Developer</h3>
             <div>
-                <Button href='./src/assets/resume.pdf' id='profileButton' variant='contained' color='error'>DOWNLOAD CV</Button>
+                <Button 
+                    variant='contained' 
+                    color='error' 
+                    onClick={handleDownload}
+                >
+                    DOWNLOAD CV
+                </Button>
                 <Button href='#' id='profileButton' variant='outlined' color='warning'>CONTACT INFO</Button>
             </div>
             <div>
-                <a href='https://profile.indeed.com/p/markn-w33vjj5' target='_blank'><img id='profileIcon' src='./src/assets/img/indeed.png'></img></a>
-                <a href='https://github.com/maktezi' target='_blank'><img id='profileIcon' src='./src/assets/img/github.png'></img></a>
-                <a href='https://www.facebook.com/mlcnierras' target='_blank'><img id='profileIcon' src='./src/assets/img/fb.png'></img></a>
+                <a href='https://profile.indeed.com/p/markn-w33vjj5' target='_blank'><img id='profileIcon' src={indeed}></img></a>
+                <a href='https://github.com/maktezi' target='_blank'><img id='profileIcon' src={github}></img></a>
+                <a href='https://www.facebook.com/mlcnierras' target='_blank'><img id='profileIcon' src={fb}></img></a>
             </div>
         </div>
     </div>
--- src/components/section/About.jsx
+++ src/components/section/About.jsx
@@ -1,4 +1,7 @@
 import './section.css'
+import aboutPic from '/src/assets/img/about-pic.png';
+import experience from '/src/assets/img/experience.png';
+import education from '/src/assets/img/education.png';
 
 const About = () => {
     return (
@@ -7,13 +10,13 @@ const About = () => {
         <div id="about-wrapper">
             
             <div id="about-wrapper-img">
-                <img className="about-img" src="./src/assets/img/about-pic.png"></img>
+                <img className="about-img" src={aboutPic}/>
             </div>
 
             <div id="box-wrapper">
                 <div className='box'>
                     <div>
-                        <img className='box-icon' src='./src/assets/img/experience.png'></img>
+                        <img className='box-icon' src={experience}/>
                         <h3 className='text-head'>Experience</h3>
                         <p className='text'>1 Year</p>
                         <p className='text'>Web Development</p>
@@ -21,7 +24,7 @@ const About = () => {
                 </div>
                 <div className='box'>
                     <div>
-                        <img className='box-icon' src='./src/assets/img/education.png'></img>
+                        <img className='box-icon' src={education}/>
                         <h3 className='text-head'>Education</h3>
                         <p className='text'>Bachelors Degree</p>
                         <p className='text'>Computer Engineering</p>
