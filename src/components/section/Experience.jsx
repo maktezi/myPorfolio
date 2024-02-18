71f96e499964ef1c116155c3ced10dc2a515093f
Mak Tezi <94935785+maktezi@users.noreply.github.com>
2024-02-18 22:27:30 +0800

    update experience section

--- src/components/section/Experience.jsx
+++ src/components/section/Experience.jsx
@@ -5,20 +5,97 @@ const Experience = () => {
     <>
         <h1 className="title">Experience</h1>
         <div className='experience-wrapper'>
-            <div className='big-box'>
+            <div className='exp-box'>
                 <h2>Website Development</h2>
-                <div className='small-box'>HTML & CSS</div>
-                <div className='small-box'>box2</div>
-                <div className='small-box'>box3</div>
-                <div className='small-box'>box4</div>
-                <div className='small-box'>box5</div>
-                <div className='small-box'>box6</div>
+                <div className='big-box'>
+                    <div className='small-box'>
+                        <div className='insmall-box'>
+                            <h3>HTML, CSS & JavaScript</h3>
+                            <p className='skill-lvl'>
+                                Intermediate
+                            </p>
+                            <p className='exp-info'><i>Adept in responsive design, interactive solutions.</i></p>
+                        </div>
+                    </div>
+                    <div className='small-box'>
+                        <div className='insmall-box'>
+                            <h3>ReactJS</h3>
+                            <p className='skill-lvl'>
+                                Basic
+                            </p>
+                            <p className='exp-info'><i>Knowledgeable in designing frontend with the use of MUI.</i></p>
+                        </div>
+                    </div>
+                    <div className='small-box'>
+                        <div className='insmall-box'>
+                            <h3>Laravel</h3>
+                            <p className='skill-lvl'>
+                                Intermediate
+                            </p>
+                            <p className='exp-info'><i>Designed and hosted several systems using this framework.</i></p>
+                        </div>
+                    </div>
+                    <div className='small-box'>
+                        <div className='insmall-box'>
+                            <h3>Wordpress</h3>
+                            <p className='skill-lvl'>
+                                Intermediate
+                            </p>
+                            <p className='exp-info'><i>Designed and hosted an e-commerce website.</i></p>
+                        </div>
+                    </div>
+                    <div className='small-box'>
+                        <div className='insmall-box'>
+                            <h3>MySQL</h3>
+                            <p className='skill-lvl'>
+                                Intermediate
+                            </p>
+                            <p className='exp-info'><i>Data modeling, querying, and optimization of applications</i></p>
+                        </div>
+                    </div>
+                    <div className='small-box'>
+                        <div className='insmall-box'>
+                            <h3>GitHub</h3>
+                            <p className='skill-lvl'>
+                                Intermediate
+                            </p>
+                            <p className='exp-info'><i>Create, and manipulate databases using SQL.</i></p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className='big-box'>
+
+            <div className='exp-box'>
                 <h2>Technical Proficiency</h2>
-                <div className='small-box'>box1</div>
-                <div className='small-box'>box2</div>
-                <div className='small-box'>box3</div>
+                <div className='big-box'>
+                    <div className='small-box'>
+                        <div className='insmall-box'>
+                            <h3>System Administration</h3>
+                            <p className='skill-lvl'>
+                                Experienced
+                            </p>
+                            <p className='exp-info'><i>nstallation, configuration, maintenance of computer systems and software.</i></p>
+                        </div>
+                    </div>
+                    <div className='small-box'>
+                        <div className='insmall-box'>
+                            <h3>Network Administration</h3>
+                            <p className='skill-lvl'>
+                                Experienced
+                            </p>
+                            <p className='exp-info'><i>Configuring Mikrotik, Ubiquiti, Cisco, Huawei, TP-Link, setup Local Network and Point-to-point setup.</i></p>
+                        </div>
+                    </div>
+                    <div className='small-box'>
+                        <div className='insmall-box'>
+                            <h3>Photo & Video Editing</h3>
+                            <p className='skill-lvl'>
+                                Experienced
+                            </p>
+                            <p className='exp-info'><i>Proficient in Adobe Photoshop, After Effects and Premiere Pro</i></p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </>
