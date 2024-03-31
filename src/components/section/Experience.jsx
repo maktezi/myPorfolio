74b633be5b358cf9b9c91b5394333ae446d8c802
Mak Tezi <94935785+maktezi@users.noreply.github.com>
2024-04-01 00:48:18 +0800

    update101

--- src/components/section/Experience.jsx
+++ src/components/section/Experience.jsx
@@ -1,4 +1,23 @@
-import './section.css'
+import './section.css';
+import laravel from '../../assets/img/logo/laravel.png';
+import html from '../../assets/img/logo/html.png';
+import css from '../../assets/img/logo/css.png';
+import js from '../../assets/img/logo/js.png';
+import chrome from '../../assets/img/logo/chrome.png';
+import vscode from '../../assets/img/logo/vscode.png';
+import hostinger from '../../assets/img/logo/hostinger.png';
+import github from '../../assets/img/logo/github.png';
+import mui from '../../assets/img/logo/mui.png';
+import mysql from '../../assets/img/logo/mysql.png';
+import netlify from '../../assets/img/logo/netlify.png';
+import vercel from '../../assets/img/logo/vercel.png';
+import nodejs from '../../assets/img/logo/nodejs.png';
+import npm from '../../assets/img/logo/npm.png';
+import wordpress from '../../assets/img/logo/wordpress.png';
+import reactjs from '../../assets/img/logo/reactjs.png';
+import vitelogo from '../../assets/img/logo/vite.png';
+import photoshop from '../../assets/img/logo/photoshop.png';
+import bootstrap from '../../assets/img/logo/bootstrap.svg';
 
 const Experience = () => {
     return (
@@ -98,6 +117,91 @@ const Experience = () => {
                     </div>
                 </div>
             </div>
+
+            <div className='exp-box'>
+                <h2 className='exp-heading'>Technologies & Tools</h2>
+                <div className='big-box'>
+                    <div className='insmall-box'>
+                        <img className='tool-logo' src={html}/>
+                        <h3 className='skill-title'>HTML</h3>
+                    </div>
+                    <div className='insmall-box'>
+                        <img className='tool-logo' src={css}/>
+                        <h3 className='skill-title'>CSS</h3>
+                    </div>
+                    <div className='insmall-box'>
+                        <img className='tool-logo' src={js}/>
+                        <h3 className='skill-title'>JavaScript</h3>
+                    </div>
+                    <div className='insmall-box'>
+                        <img className='tool-logo' src={laravel}/>
+                        <h3 className='skill-title'>Laravel</h3>
+                    </div>
+                    <div className='insmall-box'>
+                        <img className='tool-logo' src={chrome}/>
+                        <h3 className='skill-title'>Chrome</h3>
+                    </div>
+                    <div className='insmall-box'>
+                        <img className='tool-logo' src={vscode}/>
+                        <h3 className='skill-title'>VS Code</h3>
+                    </div>
+                    <div className='insmall-box'>
+                        <img className='tool-logo' src={hostinger}/>
+                        <h3 className='skill-title'>Hostinger</h3>
+                    </div>
+                    <div className='insmall-box'>
+                        <img className='tool-logo' src={bootstrap}/>
+                        <h3 className='skill-title'>Bootstrap</h3>
+                    </div>
+                    <div className='insmall-box'>
+                        <img className='tool-logo' src={mui}/>
+                        <h3 className='skill-title'>Material UI</h3>
+                    </div>
+                    <div className='insmall-box'>
+                        <img className='tool-logo' src={github}/>
+                        <h3 className='skill-title'>Github</h3>
+                    </div>
+                    <div className='insmall-box'>
+                        <img className='tool-logo' src={mysql}/>
+                        <h3 className='skill-title'>MySQL</h3>
+                    </div>
+                    <div className='insmall-box'>
+                        <img className='tool-logo' src={netlify}/>
+                        <h3 className='skill-title'>Netlify</h3>
+                    </div>
+                    <div className='insmall-box'>
+                        <img className='tool-logo' src={vercel}/>
+                        <h3 className='skill-title'>Vercel</h3>
+                    </div>
+                    <div className='insmall-box'>
+                        <img className='tool-logo' src={nodejs}/>
+                        <h3 className='skill-title'>NodeJS</h3>
+                    </div>
+                    <div className='insmall-box'>
+                        <img className='tool-logo' src={npm}/>
+                        <h3 className='skill-title'>NPM</h3>
+                    </div>
+                    <div className='insmall-box'>
+                        <img className='tool-logo' src={wordpress}/>
+                        <h3 className='skill-title'>Wordpress</h3>
+                    </div>
+                    <div className='insmall-box'>
+                        <img className='tool-logo' src={reactjs}/>
+                        <h3 className='skill-title'>ReactJS</h3>
+                    </div>
+                    <div className='insmall-box'>
+                        <img className='tool-logo' src={vitelogo}/>
+                        <h3 className='skill-title'>Vite</h3>
+                    </div>
+                    <div className='insmall-box'>
+                        <img className='tool-logo' src={photoshop}/>
+                        <h3 className='skill-title'>Photoshop</h3>
+                    </div>
+
+                </div>
+                
+            </div>
+
         </div>
     </>
     )
